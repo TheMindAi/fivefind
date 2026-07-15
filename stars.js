a6baa21594ede048d85f95fe090928cba92a5424
fivefind <foxbox2123092@gmail.com>
2026-07-16 04:02:35 +0500

    Refactor star canvas initialization and properties

--- stars.js
+++ stars.js
@@ -1,118 +1,144 @@
-const canvas = document.getElementById("stars-canvas");
-
+const canvas = document.getElementById("stars");
 const ctx = canvas.getContext("2d");
 
 
-let stars=[];
+let stars = [];
 
 let width;
 let height;
 
 
+
 function resize(){
 
-width = canvas.width = window.innerWidth;
-
-height = canvas.height = window.innerHeight;
+    width = canvas.width = window.innerWidth;
+    height = canvas.height = window.innerHeight;
 
 }
 
 
-window.addEventListener("resize",resize);
+window.addEventListener("resize", resize);
 
 resize();
 
 
 
-let amount = window.innerWidth < 600 ? 70 : 150;
+const amount = window.innerWidth < 700 ? 80 : 180;
 
 
 
-class Star{
+class Star {
 
 
-constructor(){
+    constructor(){
 
-this.x=Math.random()*width;
+        this.reset();
 
-this.y=Math.random()*height;
+    }
 
-this.size=Math.random()*2+0.5;
 
-this.speed=Math.random()*0.4+0.1;
 
-this.alpha=Math.random();
+    reset(){
+
+        this.x = Math.random() * width;
+
+        this.y = Math.random() * height;
+
+        this.size = Math.random() * 2 + 0.3;
+
+        this.speed = Math.random() * 0.35 + 0.05;
+
+        this.opacity = Math.random();
+
+        this.fade = Math.random() * 0.02 + 0.005;
+
+
+    }
+
+
+
+    update(){
+
+
+        this.y -= this.speed;
+
+
+        this.opacity += this.fade;
+
+
+
+        if(this.opacity >= 1 || this.opacity <= 0.2){
+
+            this.fade *= -1;
+
+        }
+
+
+
+        if(this.y < -10){
+
+            this.y = height + 10;
+
+            this.x = Math.random() * width;
+
+        }
+
+
+    }
+
+
+
+    draw(){
+
+
+        ctx.beginPath();
+
+
+        ctx.arc(
+
+            this.x,
+
+            this.y,
+
+            this.size,
+
+            0,
+
+            Math.PI * 2
+
+        );
+
+
+
+        ctx.shadowBlur = 15;
+
+        ctx.shadowColor = "#a855ff";
+
+
+
+        ctx.fillStyle =
+
+        `rgba(255,255,255,${this.opacity})`;
+
+
+
+        ctx.fill();
+
+
+
+        ctx.shadowBlur = 0;
+
+
+    }
+
 
 }
 
 
 
-update(){
+for(let i = 0; i < amount; i++){
 
-this.y-=this.speed;
-
-
-if(this.y<0){
-
-this.y=height;
-
-this.x=Math.random()*width;
-
-}
-
-this.alpha += (Math.random()-0.5)*0.03;
-
-
-if(this.alpha<0.2)
-this.alpha=0.2;
-
-
-if(this.alpha>1)
-this.alpha=1;
-
-
-}
-
-
-
-draw(){
-
-
-ctx.beginPath();
-
-ctx.arc(
-this.x,
-this.y,
-this.size,
-0,
-Math.PI*2
-);
-
-
-ctx.shadowBlur=15;
-
-ctx.shadowColor="#a855ff";
-
-
-ctx.fillStyle=
-`rgba(255,255,255,${this.alpha})`;
-
-
-ctx.fill();
-
-
-ctx.shadowBlur=0;
-
-
-}
-
-}
-
-
-
-for(let i=0;i<amount;i++){
-
-stars.push(new Star());
+    stars.push(new Star());
 
 }
 
@@ -120,19 +146,33 @@ stars.push(new Star());
 
 function animate(){
 
-ctx.clearRect(0,0,width,height);
+
+    ctx.clearRect(
+
+        0,
+
+        0,
+
+        width,
+
+        height
+
+    );
 
 
-stars.forEach(star=>{
 
-star.update();
+    stars.forEach(star => {
 
-star.draw();
+        star.update();
 
-});
+        star.draw();
+
+    });
 
 
-requestAnimationFrame(animate);
+
+    requestAnimationFrame(animate);
+
 
 }
 
